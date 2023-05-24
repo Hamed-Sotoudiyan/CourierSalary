8c643e8612501b7f639ab7e73f78313927647d14
Hamed Sotoudiyan <hamed.sotoudiyan@gmail.com>
2023-05-25 01:09:07 +0330

    fix request URL range data for weekly salary

--- CourierSalary/IncomeOfCouriers/views.py
+++ CourierSalary/IncomeOfCouriers/views.py
@@ -10,19 +10,19 @@ from .serializers import CourierProfileSerializer, CourierWeeklySalarySerializer
 
 class CourierWeeklySalaryListView(generics.ListAPIView):
     serializer_class = CourierWeeklySalarySerializer
-    queryset = CourierWeeklySalary.objects.all()
+
     filter_backends = [filters.OrderingFilter]
     ordering_fields = ['week_starting_date']
 
     def get_queryset(self):
-        queryset = super().get_queryset()
+        queryset = CourierWeeklySalary.objects.all()
         date_from_str = self.request.query_params.get('from_date')
         date_to_str = self.request.query_params.get('to_date')
 
         if date_from_str and date_to_str:
             from_date = datetime.strptime(date_from_str, "%Y-%m-%d").date()
             to_date = datetime.strptime(date_to_str, "%Y-%m-%d").date()
-            queryset = queryset.filter(week_starting_date=[from_date, to_date])
+            queryset = queryset.filter(week_starting_date__range=[from_date, to_date])
 
         return queryset
 

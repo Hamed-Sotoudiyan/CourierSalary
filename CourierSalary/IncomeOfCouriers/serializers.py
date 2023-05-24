55d183765c57a2726a3b0ca67b1da2a567466b62
Hamed Sotoudiyan <hamed.sotoudiyan@gmail.com>
2023-05-24 19:07:06 +0330

    add serializers.py

---
+++ CourierSalary/IncomeOfCouriers/serializers.py
@@ -0,0 +1,41 @@
+from rest_framework import serializers
+
+from .models import CourierWeeklySalary, CourierProfile, CourierDailySalary, CourierIncomeChange, Trip
+
+
+class CourierProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CourierProfile
+        fields = '__all__'
+
+
+class CourierWeeklySalarySerializer(serializers.ModelSerializer):
+    courier = CourierProfileSerializer(many=False, read_only=True)
+
+    class Meta:
+        model = CourierWeeklySalary
+        fields = ['weekly_salary', 'courier', 'week_starting_date']
+
+
+class TripSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Trip
+        fields = ['distance',
+                  'courier',
+                  'customer_type',
+                  'date',
+                  'distance_from_origin'
+                  'time_of_day',
+                  'income']
+
+
+class CourierIncomeChangeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CourierIncomeChange
+        fields = ['courier', 'amount', 'date']
+
+
+class CourierDailySalarySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CourierDailySalary
+        fields = ['courier', 'date', 'daily_salary']

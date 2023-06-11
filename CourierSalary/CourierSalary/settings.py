af1f0e80a9fd2dcc2384dd83385b87205888a746
Hamed Sotoudiyan <61743175+Hamed-Sotoudiyan@users.noreply.github.com>
2023-06-11 17:40:40 +0330

    Update settings.py

--- CourierSalary/CourierSalary/settings.py
+++ CourierSalary/CourierSalary/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-84e^x&3cok+dexa8fa3$0=zuu5alx(d594ov@i$w4#-$f-qb$e'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
